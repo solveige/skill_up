a45b08250b0d11da2c85a5549c652a3194469d66
Oksana.Mosiichuk <oksana.mosiichuk@devspiration.com>
2021-12-01 03:17:39 +0200

    update prototype test

--- ObjectMethods/Part I/proto.test.js
+++ ObjectMethods/Part I/proto.test.js
@@ -1,5 +1,19 @@
-test('Prototype vs __proto__', () => {
-    const obj = { a: 1 };
+test('prototype vs __proto__', () => {
+  const obj = { a: 1 };
 
-    expect(Object.getPrototypeOf(obj) === obj.__proto__).toBe(true);
+  expect(Object.getPrototypeOf(obj) === obj.__proto__).toBe(true);
 });
+
+test('prototype of custom class vs __proto__', () => {
+  class Rectangle {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  };
+
+  //  Rectangle.prototype: {constructor: ƒ}
+  //  Rectangle.__proto__: () { [native code] }
+
+  expect(Rectangle.prototype !== Rectangle.__proto__).toBe(true)
+})
